c92a2cecd7fdbfddd1006d1598c08abf1384a0a3
Sudhir Rao <sudhirraoh@yahoo.co.in>
2019-12-11 15:14:48 +0530

    Defined contents for Singleton

--- Singleton1112/Singleton1112/Program.cs
+++ Singleton1112/Singleton1112/Program.cs
@@ -15,7 +15,19 @@ namespace Singleton1112
 
     public class Singleton
     {
-        
+        private static Singleton _instance;
+        private Singleton() { }
+        public static Singleton Instance
+        {
+            get 
+            { 
+                if(_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
     }
 
 }
