03834f3f6b7f42e8a86713f2369969d8a8d3f73b
Sudhir Rao <sudhirraoh@yahoo.co.in>
2019-12-11 15:11:55 +0530

    second commit

--- Singleton1112/Singleton1112/Program.cs
+++ Singleton1112/Singleton1112/Program.cs
@@ -15,20 +15,7 @@ namespace Singleton1112
 
     public class Singleton
     {
-        private Singleton() { }
-        private static Singleton _instance;
-
-        public static Singleton Instance
-        {
-            get
-            {
-                if(_instance == null)
-                {
-                    _instance = new Singleton();
-                }
-                return _instance;
-            }
-        }
+        
     }
 
 }
